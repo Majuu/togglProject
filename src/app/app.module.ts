4a84f85e8b09437f7d8c6cafe98604eb4c13c7ed
Maju <artur.maj94@gmail.com>
2018-10-18 11:36:06 +0200

    Start-screen 80% finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarStartComponent } from './navbar-start/navbar-start.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarStartComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
