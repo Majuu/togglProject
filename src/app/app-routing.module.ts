5d6c2024f54fdba9525d1ddba30592a0dc339a18
Maju <artur.maj94@gmail.com>
2018-11-20 18:49:30 +0100

    Next Changes + Create project styles started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {StartScreenComponent} from './logging-start/start-screen/start-screen.component';
-import {SignUpComponent} from './logging-start/sign-up/sign-up.component';
-import {LeftMenuComponent} from './running-app/left-menu/left-menu.component';
-import {TimerComponent} from './running-app/timer/timer.component';
-import {DashboardComponent} from './running-app/dashboard/dashboard.component';
-import {LoggingStartComponent} from './logging-start/logging-start.component';
-import {RunningAppComponent} from './running-app/running-app.component';
+import {StartScreenComponent} from './auth/start-screen/start-screen.component';
+import {SignUpComponent} from './auth/sign-up/sign-up.component';
+import {LeftMenuComponent} from './src/left-menu/left-menu.component';
+import {TimerComponent} from './src/timer/timer.component';
+import {DashboardComponent} from './src/dashboard/dashboard.component';
+import {LoggingStartComponent} from './auth/logging-start.component';
+import {RunningAppComponent} from './src/running-app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarStartComponent } from './logging-start/navbar-start/navbar-start.component';
-import { StartScreenComponent } from './logging-start/start-screen/start-screen.component';
-import { SignUpComponent } from './logging-start/sign-up/sign-up.component';
+import { NavbarStartComponent } from './auth/navbar-start/navbar-start.component';
+import { StartScreenComponent } from './auth/start-screen/start-screen.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
-import { LeftMenuComponent } from './running-app/left-menu/left-menu.component';
-import { TimerComponent } from './running-app/timer/timer.component';
-import { DashboardComponent } from './running-app/dashboard/dashboard.component';
-import { LoggingStartComponent } from './logging-start/logging-start.component';
-import { RunningAppComponent } from './running-app/running-app.component';
+import { LeftMenuComponent } from './src/left-menu/left-menu.component';
+import { TimerComponent } from './src/timer/timer.component';
+import { DashboardComponent } from './src/dashboard/dashboard.component';
+import { LoggingStartComponent } from './auth/logging-start.component';
+import { RunningAppComponent } from './src/running-app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
